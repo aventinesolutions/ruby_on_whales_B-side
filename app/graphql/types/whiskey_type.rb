a1823336c911d8fe277bdb4db058ad1d41c27c2e
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-10 15:03:33 +0100

    :bear: :construction: add the photo URL to the Whiskey type
    202003062033 add GraphQL API

--- app/graphql/types/whiskey_type.rb
+++ app/graphql/types/whiskey_type.rb
@@ -1,8 +1,16 @@
 module Types
   # the GraphQl type for Whiskeys
   class WhiskeyType < Types::BaseObject
+    include Rails.application.routes.url_helpers
+
     field :id, ID, null: false
-    field :description, String, null: false
-    field :price, Float, null: false
+    field :description, String, null: true
+    field :price, Float, null: true
+    field :photo_url, String, null: true
+
+    def photo_url
+      return unless object.photo.attached?
+      rails_blob_url(object.photo, only_path: true)
+    end
   end
 end
