3648edeb07ecf9c195cdf03ec16f2c973433fff1
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-10 20:33:52 +0100

    :bear: :construction: create the Apollo Provider container for React
    202003101747 Whiskey components for React

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,1 +1,5 @@
-console.log('*** React booted ***');
+import React from 'react';
+import { render } from 'react-dom';
+import Provider from '../components/Provider';
+
+render(<Provider>Apollo provider booted</Provider>, document.querySelector('#root'));
