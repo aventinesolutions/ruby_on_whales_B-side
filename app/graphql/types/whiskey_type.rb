7dc3c8ce49341c0ba612c76ee429e013de5d4325
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-10 15:11:35 +0100

    :bear: :construction: format the price field to Euro currency :euro:
    202003062033 add GraphQL API

--- app/graphql/types/whiskey_type.rb
+++ app/graphql/types/whiskey_type.rb
@@ -2,11 +2,16 @@ module Types
   # the GraphQl type for Whiskeys
   class WhiskeyType < Types::BaseObject
     include Rails.application.routes.url_helpers
+    include ActionView::Helpers::NumberHelper
 
     field :id, ID, null: false
     field :description, String, null: true
-    field :price, Float, null: true
+    field :price, String, null: true
     field :photo_url, String, null: true
+
+    def price
+      number_to_currency(object.price, unit: '€ ')
+    end
 
     def photo_url
       return unless object.photo.attached?
