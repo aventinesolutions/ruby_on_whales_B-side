6050fdc3d53e716bda237e91bad2b07c11ee90f9
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-13 14:01:11 +0100

    :bear: :construction: add quality to model, test and factory
    202003131340 add Quality to Ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,6 @@
 # Rating model for a given Whiskey and a given Account
 class Rating < ApplicationRecord
+  enum quality: %i[taste color smokiness]
   enum stars: %i[no_stars one two three four five]
   belongs_to :account
   belongs_to :whiskey
