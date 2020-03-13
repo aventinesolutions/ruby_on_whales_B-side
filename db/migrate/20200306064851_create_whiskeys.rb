b8824e5be3e4eba5ea7d6970ef2de31f708ee1ec
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-13 12:52:44 +0100

    :bear: :construction: add title to the database table
    202003131242 add title to Whiskey model

--- db/migrate/20200306064851_create_whiskeys.rb
+++ db/migrate/20200306064851_create_whiskeys.rb
@@ -2,9 +2,12 @@
 class CreateWhiskeys < ActiveRecord::Migration[6.0]
   def change
     create_table :whiskeys, id: :uuid do |t|
+      t.string :title, null: false
       t.string :description
       t.decimal :price, default: 0.0, null: false, precision: 12, scale: 2
       t.timestamps
     end
+
+    add_index :whiskeys, :title
   end
 end
