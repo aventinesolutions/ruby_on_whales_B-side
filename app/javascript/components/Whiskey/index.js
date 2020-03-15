62b0c54932a592f4f821701051a1d84c3f9feeef
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-15 18:12:16 +0100

    :bear: :construction: add the Search component :two:
    202003151456 Search component

--- app/javascript/components/Whiskey/index.js
+++ app/javascript/components/Whiskey/index.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { AccountContext } from "../../context/AccountContext";
 import ReactImageAppear from 'react-image-appear';
 import './styles.scss';
 
 const Whiskey = ({ id, title, description, price, photoUrl }) =>
-  <AccountContext.Consumer>
-    {
-      ({ account_id }) =>
-        <div className='whiskey-container' key={id}>
-          <h2 className='whiskey-title'>{title}</h2>
-          <div className='whiskey-description'>{description}</div>
-          <div className='whiskey-price'>Price: <span>{price}</span></div>
-          <ReactImageAppear
-            className="whiskey-photo"
-            src={photoUrl}
-            placeholder
-            animation='bounceInDown'
-            animationDuration='2s'
-          />
-          <div style={{ display: 'none' }}>Account ID: {account_id}</div>
-        </div>
-    }
-  </AccountContext.Consumer>
+  <div className='whiskey-container' key={id}>
+    <h2 className='whiskey-title'>{title}</h2>
+    <div className='whiskey-description'>{description}</div>
+    <div className='whiskey-price'>Price: <span>{price}</span></div>
+    <ReactImageAppear
+      className="whiskey-photo"
+      src={photoUrl}
+      placeholder
+      animation='bounceInDown'
+      animationDuration='2s'
+    />
+  </div>
 ;
 
 Whiskey.displayName = 'Whiskey';
