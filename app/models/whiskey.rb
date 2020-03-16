007c15f870863fa9e5a63de1e5530e3cd004f562
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-16 16:20:07 +0100

    :bear: :construction: add ratings filters to model and search query
    202003161553 filter by Ratings

--- app/models/whiskey.rb
+++ app/models/whiskey.rb
@@ -5,9 +5,9 @@ class Whiskey < ApplicationRecord
   has_many :ratings, inverse_of: :whiskey, dependent: :destroy
   pg_search_scope :search_text, against: %i[title description]
 
-  scope :minimum_5stars, -> { joins(:ratings).where(ratings: { stars: :five }).uniq }
-  scope :minimum_4stars, -> { joins(:ratings).where(ratings: { stars: %i[four five] }).uniq }
-  scope :minimum_3stars, -> { joins(:ratings).where(ratings: { stars: %i[three four five] }).uniq }
-  scope :minimum_2stars, -> { joins(:ratings).where(ratings: { stars: %i[two three four five] }).uniq }
-  scope :minimum_1star, -> { joins(:ratings).where(ratings: { stars: %i[one two three four five] }).uniq }
+  scope :minimum_5stars, -> { joins(:ratings).where(ratings: { stars: :five }) }
+  scope :minimum_4stars, -> { joins(:ratings).where(ratings: { stars: %i[four five] }) }
+  scope :minimum_3stars, -> { joins(:ratings).where(ratings: { stars: %i[three four five] }) }
+  scope :minimum_2stars, -> { joins(:ratings).where(ratings: { stars: %i[two three four five] }) }
+  scope :minimum_1star, -> { joins(:ratings).where(ratings: { stars: %i[one two three four five] }) }
 end
