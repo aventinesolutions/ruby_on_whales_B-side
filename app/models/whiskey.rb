320831f58033c2768af38f8029801fde6d9dbfa7
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-16 16:03:32 +0100

    :bear: :construction: add scopes to Whiskey model
    202003161553 filter by Ratings

--- app/models/whiskey.rb
+++ app/models/whiskey.rb
@@ -4,4 +4,10 @@ class Whiskey < ApplicationRecord
   has_one_attached :photo
   has_many :ratings, inverse_of: :whiskey, dependent: :destroy
   pg_search_scope :search_text, against: %i[title description]
+
+  scope :minimum_5stars, -> { joins(:ratings).where(ratings: { stars: :five }).uniq }
+  scope :minimum_4stars, -> { joins(:ratings).where(ratings: { stars: %i[four five] }).uniq }
+  scope :minimum_3stars, -> { joins(:ratings).where(ratings: { stars: %i[three four five] }).uniq }
+  scope :minimum_2stars, -> { joins(:ratings).where(ratings: { stars: %i[two three four five] }).uniq }
+  scope :minimum_1star, -> { joins(:ratings).where(ratings: { stars: %i[one two three four five] }).uniq }
 end
