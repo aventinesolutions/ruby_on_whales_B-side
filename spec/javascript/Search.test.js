8d612a937dddf9fe782ed332ec9453518a26ed6e
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-24 20:26:10 +0100

    :bear: :construction: trying to use JSDOM with Enzyme
    202003241840 more snapshot tests

--- spec/javascript/Search.test.js
+++ spec/javascript/Search.test.js
@@ -1,17 +1,21 @@
+const { JSDOM } = require('jsdom');
+const jsdom = new JSDOM(`<!doctype html><html>
+   <body><div id="root" data-account-id="${fake('random.uuid')}"/></body>
+</html>`);
+const { window } = jsdom;
+global.window = window;
+global.document = window.document;
+
 import React from 'react';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
-import { fake } from 'faker';
 
-import { MockedProvider } from '@apollo/react-testing';
 import Search from '../../app/javascript/components/Search';
 
 describe('Search', () => {
   it('renders a Search component', () => {
     const output = shallow(
-      <MockedProvider>
-        <Search />
-      </MockedProvider>
+      <Search />
     );
     expect(shallowToJson(output)).toMatchSnapshot();
   });
