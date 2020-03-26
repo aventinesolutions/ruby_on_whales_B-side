c3e7e7ee6d44265ab73b49eb74fea6ca0d687fd2
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-26 18:15:35 +0100

    :bear: :construction: some refactoring of the Whiskey snapshot test
    202003241840 more snapshot tests

--- spec/javascript/Whiskey.test.js
+++ spec/javascript/Whiskey.test.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
-import { fake } from 'faker';
 
 import Whiskey from '../../app/javascript/components/Whiskey';
 
+let output;
+
+beforeEach(() => {
+  output = shallow(
+    <Whiskey
+      id="28846727-a4a4-4a8a-8a2c-2a5098b8a397"
+      title="Licensed Wooden Keyboard"
+      description="Expanded methodical definition"
+      price="€170,00"
+      photoUrl="http://lorempixel.com/640/480"
+      ratings={[
+        { quality: 'taste', stars: 'one' },
+        { quality: 'color', stars: 'five' },
+        { quality: 'smokiness', stars: 'three' },
+      ]}
+    />
+  );
+});
+
 describe('Whiskey', () => {
   it('renders a Whiskey component', () => {
-    const output = shallow(
-      <Whiskey
-        id={fake("random:uuid")}
-        title={fake("commerce:productName")}
-        description={fake("company:catchPhrase")}
-        price={fake("commerce:price")}
-        photoUrl={fake("image:imageUrl")}
-        ratings={[
-          { quality: 'taste', stars: 'one' },
-          { quality: 'color', stars: 'five' },
-          { quality: 'smokiness', stars: 'three' },
-        ]}
-      />
-    );
     expect(shallowToJson(output)).toMatchSnapshot();
   });
 });
