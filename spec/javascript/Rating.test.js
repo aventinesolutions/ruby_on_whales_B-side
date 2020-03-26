0ff37a46d812219d719a84772dd80944c4a84ba4
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-26 18:23:47 +0100

    :bear: :construction: some refactoring of the Rating snapshot test
    202003241840 more snapshot tests

--- spec/javascript/Rating.test.js
+++ spec/javascript/Rating.test.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
-import { fake } from 'faker';
 
 import { MockedProvider } from '@apollo/react-testing';
 import Rating from '../../app/javascript/components/Rating';
 
+let output;
+
+beforeEach(() => {
+  output = shallow(
+    <MockedProvider>
+      <Rating
+        id="0feeb520-8384-4985-b631-0127ddb56a47"
+        quality="taste"
+        ratings={[{ quality: 'taste', stars: 'three' }]}
+        account_id="7eac9c4e-90e1-428b-bda8-0203b75dd0ed" />
+    </MockedProvider>
+  );
+});
+
 describe('Rating', () => {
   it('renders a Rating component', () => {
-    const output = shallow(
-      <MockedProvider>
-        <Rating
-          id={fake("random.uuid")}
-          quality="taste"
-          ratings={[{ quality: 'taste', stars: 'three' }]}
-          account_id={fake("random.uuid")} />
-      </MockedProvider>
-    );
     expect(shallowToJson(output)).toMatchSnapshot();
   });
 });
