bb78d62797f59640db392f166e6353fb75461806
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-26 18:18:34 +0100

    :bear: :construction: some refactoring of the Search snapshot test
    202003241840 more snapshot tests

--- spec/javascript/Search.test.js
+++ spec/javascript/Search.test.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
-import { fake } from 'faker';
+
+const fake = require('faker');
 
 import Search from '../../app/javascript/components/Search';
 import AccountContext from '../../app/javascript/context/AccountContext';
 
+let output;
+
+beforeEach(() => {
+  output = shallow(
+    <AccountContext.Provider value={{ account_id: fake.random.uuid() }}>
+      <Search />
+    </AccountContext.Provider>
+  );
+});
+
 describe('Search', () => {
   it('renders a Search component', () => {
-    const output = shallow(
-      <AccountContext.Provider value={{ account_id: fake("random:uuid") }}>
-        <Search />
-      </AccountContext.Provider>
-    );
     expect(shallowToJson(output)).toMatchSnapshot();
   });
 });
