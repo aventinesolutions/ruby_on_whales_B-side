13881a6870c5c1c3d9ac9bef4ef47ad7e1571da3
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-27 15:17:00 +0100

    :bear: :construction: starting to test reaction to changes in Ratings
    202003241840 more snapshot tests

--- spec/javascript/Rating.test.js
+++ spec/javascript/Rating.test.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 
 import { MockedProvider } from '@apollo/react-testing';
 import Rating from '../../app/javascript/components/Rating';
 
-let output;
+describe('shallow rendering Rating', () => {
+  let output;
 
-beforeEach(() => {
-  output = shallow(
-    <MockedProvider>
-      <Rating
-        id="0feeb520-8384-4985-b631-0127ddb56a47"
-        quality="taste"
-        ratings={[{ quality: 'taste', stars: 'three' }]}
-        account_id="7eac9c4e-90e1-428b-bda8-0203b75dd0ed" />
-    </MockedProvider>
-  );
-});
+  beforeEach(() => {
+    output = shallow(
+      <MockedProvider>
+        <Rating
+          id="0feeb520-8384-4985-b631-0127ddb56a47"
+          quality="taste"
+          ratings={[{ quality: 'taste', stars: 'three' }]}
+          account_id="7eac9c4e-90e1-428b-bda8-0203b75dd0ed" />
+      </MockedProvider>
+    );
+  });
 
-describe('Rating', () => {
   it('renders a Rating component', () => {
     expect(shallowToJson(output)).toMatchSnapshot();
+  });
+});
+
+describe('reacts to changes of Ratings', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(
+      <MockedProvider>
+        <Rating
+          id="0feeb520-8384-4985-b631-0127ddb56a47"
+          quality="taste"
+          ratings={[{ quality: 'taste', stars: 'two' }]}
+          account_id="7eac9c4e-90e1-428b-bda8-0203b75dd0ed" />
+      </MockedProvider>
+    );
+  });
+
+  it('reacts to changes in Ratings', () => {
+    expect(wrapper.find('Widget').first().props().selectedRating).toEqual(2);
   });
 });
