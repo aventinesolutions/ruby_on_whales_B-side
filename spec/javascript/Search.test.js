180011d2579c7e42f5528971b797538f0788193f
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-30 16:01:55 +0200

    :bear: :construction: failing test with mocked Apollo provider for search query
    202003241840 more snapshot tests

--- spec/javascript/Search.test.js
+++ spec/javascript/Search.test.js
@@ -1,24 +1,55 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
+import { act } from 'react-dom/test-utils';
 
 const fake = require('faker');
+const wait = require('waait');
 
 import Search from '../../app/javascript/components/Search';
 import AccountContext from '../../app/javascript/context/AccountContext';
+import { MockedProvider } from "@apollo/react-testing";
 
-let output;
+describe('shallow rendering Search component', () => {
+  let output;
 
-beforeEach(() => {
-  output = shallow(
-    <AccountContext.Provider value={{ account_id: fake.random.uuid() }}>
-      <Search />
-    </AccountContext.Provider>
-  );
-});
+  beforeEach(() => {
+    output = shallow(
+      <AccountContext.Provider value={{ account_id: fake.random.uuid() }}>
+        <Search />
+      </AccountContext.Provider>
+    );
+  });
 
-describe('Search', () => {
   it('renders a Search component', () => {
     expect(shallowToJson(output)).toMatchSnapshot();
+  });
+});
+
+describe('reacts to changes in Search filters', () => {
+  let component;
+  let queryCalled;
+  let mocks;
+
+  beforeEach(() => {
+    queryCalled = false;
+    mocks = {};
+    component = mount(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <AccountContext.Provider value={{ account_id: fake.random.uuid() }}>
+          <Search />
+        </AccountContext.Provider>
+      </MockedProvider>
+    );
+  });
+
+  it('reacts to changes in filters', async () => {
+    expect(component.find('#search').props().value).toEqual("");
+    await act(async () => {
+      component.find('#search').props().value = "casks";
+    });
+    await wait(0);
+    expect(component.find('#search').props().value).toEqual("casks");
+    expect(queryCalled).toBeTruthy();
   });
 });
