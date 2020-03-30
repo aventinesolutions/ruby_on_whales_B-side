a3db006ac4a47d8ca91f80ad591ea2e37bd8fad0
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2020-03-30 17:13:06 +0200

    :bear: :construction: failing test for returning results from Search query
    202003241840 more snapshot tests

--- spec/javascript/Search.test.js
+++ spec/javascript/Search.test.js
@@ -6,7 +6,7 @@ import { act } from 'react-dom/test-utils';
 const fake = require('faker');
 const wait = require('waait');
 
-import Search from '../../app/javascript/components/Search';
+import Search, { SEARCH_WHISKEYS_QUERY } from '../../app/javascript/components/Search';
 import AccountContext from '../../app/javascript/context/AccountContext';
 import { MockedProvider } from "@apollo/react-testing";
 
@@ -28,12 +28,56 @@ describe('shallow rendering Search component', () => {
 
 describe('reacts to changes in Search filters', () => {
   let component;
+  let accountId;
   let queryCalled;
   let mocks;
 
   beforeEach(() => {
     queryCalled = false;
-    mocks = {};
+    accountId = "cdbdb637-41f7-435a-b32f-9182fbbf6329";
+    mocks = [
+      {
+        request: {
+          query: SEARCH_WHISKEYS_QUERY,
+          variables: {
+            accountId: accountId,
+            text: "casks",
+            ratingsFilter: ""
+          }
+        },
+        result: () => {
+          queryCalled = true;
+          return {
+            loading: false,
+            data: {
+              search: [
+                {
+                  id: "6c7e652d-5abe-487c-bcd3-f5410437fd92",
+                  title: "Practical Wooden Gloves",
+                  description: "Programmable system-worthy process improvement",
+                  price: "€ 99.00",
+                  photoUrl: "https://source.unsplash.com/640x480",
+                  ratings: [
+                    {
+                      id: "259ac39c-a7cb-40dd-9c06-7ce431618962",
+                      accountId: accountId,
+                      quality: "smokiness",
+                      stars: "three"
+                    },
+                    {
+                      id: "4a9fcf69-82ca-4026-881f-24dec463f14d",
+                      accountId: accountId,
+                      quality: "color",
+                      stars: "five"
+                    }
+                  ]
+                }
+              ]
+            }
+          };
+        }
+      }
+    ];
     component = mount(
       <MockedProvider mocks={mocks} addTypename={false}>
         <AccountContext.Provider value={{ account_id: fake.random.uuid() }}>
@@ -47,6 +91,7 @@ describe('reacts to changes in Search filters', () => {
     expect(component.find('#search').props().value).toEqual("");
     await act(async () => {
       component.find('#search').props().value = "casks";
+      await component.find('#search').props().onChange({ target: { value: "casks" } });
     });
     await wait(0);
     expect(component.find('#search').props().value).toEqual("casks");
